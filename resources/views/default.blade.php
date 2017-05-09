779dde68a8ccc6582d6b9f7e23d0a5447cfbb2a5
slachlol54 <slachlol54@gmail.com>
2017-05-09 14:30:21 +0200

    commit

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -5,11 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Socopec</title>
+    <link rel="stylesheet" type="text/css" href="{{url('/css/app.css')}}">
     <link rel="stylesheet" type="text/css" href="{{url('/css/style.css')}}">
-
-
-
 
 </head>
 <body>
@@ -17,12 +15,21 @@
     <div class="header">
         <div class="row">
             <div class="col-md-4">
-                <img src="{{url('/images/logo.png')}}" alt="Image"/>
+                <div class="logo">
+                    <img src="{{url('/images/logo.png')}}" alt="Image"/>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="header_tittle">
+                    <p>Socopec</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="deconnexion">
+                        <span class="glyphicon glyphicon-user"></span>Déconnexion
+                </div>
             </div>
 
-            <div class="header_tittle">
-                <p>Socopec</p>
-            </div>
         </div>
     </div>
     <div class="header_bottom">
