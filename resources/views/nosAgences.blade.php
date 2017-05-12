a30c6d92eda1a981e43d2e7fe87132c7ae0d6770
slachlol54 <slachlol54@gmail.com>
2017-05-12 11:17:59 +0200

    commit vue NosAgence

--- resources/views/nosAgences.blade.php
+++ resources/views/nosAgences.blade.php
@@ -8,12 +8,21 @@
             <h1>NOS AGENCES</h1>
         </div>
         <div class="nosAgence">
+            <div class="agence">
+                <div class="divPhotoAgence">
+                    {{ Html::image('images/agence.jpg', 'a picture', array('class' => 'photoAgence')) }}
+                </div>
+                <div class="titreAgence">Agence Lorraine</div>
+                <div class="adresseAgence">testqzdqzdqzdqzdqzdqzdqzdqzdqzdqzdqzdqzdqzdqzdqzdqzd</div>
+                <div class="divButtonVoir"><button class="buttonVoir">{{ Html::image('images/eye.png') }}Voir</button></div>
+            </div>
             <div class="agence"></div>
             <div class="agence"></div>
             <div class="agence"></div>
-            <div class="agence"></div>
-
         </div>
+
+
+    </div>
     </div>
 
 
