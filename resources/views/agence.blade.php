6a287d92576d15b7cf4e014b5cb1923afdf4330b
Thibaut Silva <silva.thibaut@gmail.com>
2017-05-29 16:42:32 +0200

    fix vehicule

--- resources/views/agence.blade.php
+++ resources/views/agence.blade.php
@@ -33,10 +33,10 @@
                             <div class="infoVehicule">{{$voiture->largeur}}</div>
                             <div class="infoVehicule">{{$voiture->poids}}</div>
                         </div>
-                        <div class="divReserverVehicule">
+      {{--                  <div class="divReserverVehicule">
                             <button class="btn btn-primary btnReserverVehicule">Choisir ce véhicule</button>
-                        </div>
-                        {{--<div class="divReserverVehicule"><a href="{{ url('/vehicule/' . $voiture->id) }}" ><button class="btn btn-primary btnReserverVehicule">Choisir ce véhicule</button></a></div>--}}
+                        </div>--}}
+                        <div class="divReserverVehicule"><a href="{{ url('/vehicule/' . $voiture->id) }}" ><button class="btn btn-primary btnReserverVehicule">Choisir ce véhicule</button></a></div>
                     </div>
 
                 @endforeach
